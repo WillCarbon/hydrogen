a09a6811d48e2fb0cf1f654f925d8097ed1e3e46
Matt Nicholls <matt@carboncreative.net>
2022-02-08 17:03:35 +0000

    Added template functions for custom post types (redirect + posts_per_page)

--- web/wp-content/themes/themename/lib/PostType/ExamplePostType.php
+++ web/wp-content/themes/themename/lib/PostType/ExamplePostType.php
@@ -10,12 +10,19 @@ class ExamplePostType extends BasePostType implements PostTypeInterface
 {
 
     /**
-     * ExamplePostType constructor.
+     * Post type constructor.
      */
     public function __construct()
     {
         add_action('init', [$this, 'register']);
+
+        /** uncomment to set a custom PostsPerPage for the archives */
+        //add_action( 'pre_get_posts', [$this, 'setPostsPerPage']);
+
+        /** uncomment to redirect unwanted pages to the post type archive */
+        //add_action('wp', [$this, 'getRedirect']);
     }
+
 
     /**
      * Register post type
@@ -39,5 +46,32 @@ class ExamplePostType extends BasePostType implements PostTypeInterface
             ]
         ]);
     }
+
+
+    /**
+     * Set archives posts per page
+     *
+     * @param WP_Query $query
+     * @return void
+     */
+    public function setPostsPerPage($query){
+        if ( !is_admin() && $query->is_main_query() ):
+            if ( is_post_type_archive('example') || is_tax('example_cat') ):
+                $query->set('posts_per_page', 10);
+            endif;
+        endif;
+    }
+
+
+    /**
+     * Redirect single post and taxonomy pages to archive
+     */
+    public function getRedirect()
+    {
+        if (is_singular('example') || is_tax('example_cat')) {
+            wp_redirect(get_post_type_archive_link('example'));
+        }
+    }
+
 }
 (new ExamplePostType());

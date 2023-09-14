664d7b9b2d1973aa6a00013d9a80dd4450dde41d
Ali <ali@carboncreative.net>
2023-09-14 14:18:34 +0100

    Adding SVG Sprite Symbol package - removing need for Gulp

---
+++ svgo.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    multipass: true, // boolean. false by default
+    datauri: 'enc', // 'base64' (default), 'enc' or 'unenc'.
+    js2svg: {
+      indent: 2, // string with spaces or number of spaces. 4 by default
+      pretty: true, // boolean, false by default
+    },
+    plugins: [
+        {
+          name: 'preset-default',
+          params: {
+            overrides: {
+              // customize default plugin options
+              inlineStyles: {
+                onlyMatchedOnce: false,
+              },
+    
+              // or disable plugins
+              removeDoctype: false,
+            },
+          },
+        },
+      ],
+  };
